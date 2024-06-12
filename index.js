3027d49e32ba847eb8c84ba4ae1964d1191fe0cf
Funprod <131615685+Funprod@users.noreply.github.com>
2024-06-12 17:25:59 +0300

    04.01

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ let timerId; // переменная, которая будет хранить I
 
 const startButton = document.getElementById("start");
 startButton.addEventListener("click", function () {
-  // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ setInterval
-  timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
+  if (!timerId) {
+    timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
+  }
 });
 
 const stopButton = document.getElementById("stop");
 stopButton.addEventListener("click", function () {
-  // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ clearInterval
-  clearInterval(timerId); // останавливаем таймер
+  if (timerId) {
+    clearInterval(timerId); // останавливаем таймер
+    timerId = null;
+  }
 });
 
 function updateClock() {
   const clock = document.getElementById("clock");
-  // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ new Date()
   const now = new Date();
   const hours = now.getHours();
   const minutes = now.getMinutes();
